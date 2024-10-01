cac843a679cbafdb90720977473a7d8f5963054c
Henrique <henriquekr23@gmail.com>
2024-10-01 00:05:44 -0300

    Update main.c

--- main.c
+++ main.c
@@ -41,8 +41,8 @@ void menu(){
 
     int funcionalidade;
     do{
-    printf("\n-==============\n-=| MERCADO |=-\n-=============-\n\n");
-    printf("(1) Cadastrar Produtos;\n(2) Listar Produtos;\n(3) Comprar Produtos;\n(4) Visualizar Carrinho;\n(5) Fechar Pedido;\n(6) Sair do Sistema;\n");
+    printf("\n\n-=============-\n-=| MERCADO |=-\n-=============-\n\n");
+    printf("(1) Cadastrar Produtos;\n(2) Listar Produtos;\n(3) Comprar Produtos;\n(4) Visualizar Carrinho;\n(5) Informacoes Produtos;\n(6) Fechar Pedido;\n(7) Sair do Sistema;\n");
     printf("\nDigite a funcionalidade desejada: ");
     scanf("%d", &funcionalidade);
         switch (funcionalidade){
@@ -58,18 +58,22 @@ void menu(){
         case 4: 
             visualizarCarrinho();
             break;
-        case 5: 
+        case 5:
+            infoProduto();
+            break;
+        case 6: 
             fecharPedido();
             break;
-        case 6:
+        case 7:
             break;
         default:
             printf("\nNumero Invalido\n\n");
             break;
         }
 
-    } while (funcionalidade != 6 );
+    } while (funcionalidade != 7 );
 }
+
 void cadastrarProdutos(){
     //Permite o cadastro de novos produtos.
 
@@ -77,7 +81,7 @@ void cadastrarProdutos(){
     printf("\n-== Cadastro de Produtos ==-\n");
     printf("Digite quantos produtos deseja cadastrar: ");
     scanf("%d", &quantidade);
-    if (quantidade + totalProdutos < MAX){
+    if (quantidade + totalProdutos < MAX+1){
         for (int i = 0; i < quantidade; i++){
             printf("Nome do produto %d: ", i+1);
             scanf(" %29[^\n]s", p[i].nome);
@@ -88,44 +92,46 @@ void cadastrarProdutos(){
             p[totalProdutos++];
         }
     } else {
-        printf("Quantidade invalida, espaco sobrando para cadastro: %d\n", 50 - totalProdutos);
+        printf("Quantidade invalida, espaco sobrando para cadastro: %d", 50 - totalProdutos);
     }
 }
+
 void listarProdutos(){
     //Exibe todos os produtos cadastrados.
     //A listagem de produtos e a visualização do carrinho devem ser realizadas de forma clara, exibindo informações detalhadas.
 
-    int sair; //Variavel auxiliar
+    int sair, funcionalidade; //Variaveis auxiliares
     printf("\n-== Produtos Cadastrados ==-\n");
     for (int i = 0; i < totalProdutos; i++){
         printf("Nome: %s | Preco: %.2f | Codigo: %d\n", p[i].nome, p[i].preco, p[i].codigo);
     }
 }
+
 void comprarProduto(){
     //Adiciona produtos ao carrinho.
     //Um produto só pode ser adicionado ao carrinho se já estiver cadastrado no sistema.
     //Produtos que já estão no carrinho devem ter suas quantidades aumentadas ao invés de serem duplicados.
 
-    int codigo, condicao;
+    int codigo, verificacao;
     printf("\n-== Comprar Produtos ==-\n");
-    printf("Digite o codigo do produto que deseja cadastrar (Caso nao saiba, digite 0 para listar os produtos): ");
+    printf("Digite o codigo do produto que deseja cadastrar (Digite 0 para listar os produtos): ");
     scanf("%d", &codigo);
     if (codigo == 0){
         listarProdutos();
         comprarProduto();
     }else{
-        Produto *p = pegarProdutoPorCodigo(codigo);
-        if (p != NULL){
-        condicao = temNoCarrinho(codigo);
-            if (condicao != -1){
-                c[condicao].quantidade++;
-            } else {
-                if (totalCarrinho < MAX){
-                    c[totalCarrinho].produto = *p;
-                    c[totalCarrinho].quantidade = 1;
-                    totalCarrinho++;
+        Produto *p = pegarProdutoPorCodigo(codigo); //Variavel p recebe endereco do produto
+        if (p != NULL){ // Se o produto existir
+        verificacao = temNoCarrinho(codigo); // Variavel recebe o valor retornado da funcao temNoCarrinho
+            if (verificacao != -1){ // Se o valor for diferente de 1
+                c[verificacao].quantidade++; // Quantidade do produto +1
+            } else { // Se for igual a 1
+                if (totalCarrinho < MAX){ // Se o totalCarrinho for menor que 50
+                    c[totalCarrinho].produto = *p; // Posicao referente ao totalCarrinho(ultimo valor do vetor) recebe o conteudo do produto
+                    c[totalCarrinho].quantidade = 1; // E sua quantidade e 1
+                    totalCarrinho++; // Total do carrinho +1
                 } else {
-                    printf("Carrinho cheio!");
+                    printf("Carrinho cheio!"); // Se for maior que 50 
                 }
             }
             printf("Produto adicionado ao carrinho\n");
@@ -135,41 +141,45 @@ void comprarProduto(){
     }
     
 }
+
 void fecharPedido(){
     //Calcula e exibe o valor total da compra e esvazia o carrinho.
     //Ao fechar o pedido, o carrinho deve ser esvaziado, e o sistema deve retornar ao menu principal.
-
+    float total = 0, opcao = 0; //Variavel auxiliar
+    printf("\n-== Fechamento de Pedido ==-\n");
+    for (int i = 0; i < totalCarrinho; i++){
+        if (c[i].quantidade != 1){
+            total += c[i].quantidade*c[i].produto.preco;
+        } else {
+            total += c[i].produto.preco;
+        }
+        printf("Nome: %s | Codigo: %d | Quantidade: %d | Subtotal: %.2f\n", c[i].produto.nome, c[i].produto.codigo, c[i].quantidade , total);
+    }
+    printf("Valor total do Carrinho: %.2f\n", total);
+    printf("Digite 1 para esvaziar o carrinho: ");
+    scanf("%d", &opcao);
 }
+
 void visualizarCarrinho(){
     //Exibe os produtos no carrinho.
     //A listagem de produtos e a visualização do carrinho devem ser realizadas de forma clara, exibindo informações detalhadas.
     int funcionalidade; //Variavel auxiliar
     printf("\n-== Carrinho ==-\n");
     for (int i = 0; i < totalCarrinho; i++){
-        printf("Nome: %s | Preco: %.2f | Codigo: %d\n", c[i].produto.nome, c[i].produto.preco, c[i].produto.codigo);
-    }
-    printf("\n(1) Menu\n(2) Fechar Pedido\nDigite a funcionalidade desejada: ");
-    scanf("%d", &funcionalidade);
-    switch (funcionalidade){
-    case 1:
-        break;
-    case 2:
-        fecharPedido();
-        break;
-    default:
-        printf("Numero invalido");
-        break;
+        printf("Nome: %s | Preco: %.2f | Codigo: %d | Quantidade: %d\n", c[i].produto.nome, c[i].produto.preco, c[i].produto.codigo, c[i].quantidade);
     }
 }
+
 int temNoCarrinho(int codigo){
     //Verifica se o produto já está no carrinho.
     for (int i = 0; i < totalCarrinho; i++){
         if (c[i].produto.codigo == codigo){
-            return i;
+            return 1;
         }
     }
     return -1;
 }
+
 Produto *pegarProdutoPorCodigo(int codigo){
     //Retorna um produto a partir do código informado.
     for (int i = 0; i < totalProdutos; i++){
@@ -177,11 +187,22 @@ Produto *pegarProdutoPorCodigo(int codigo){
     }
     return NULL;
 }
+
 void infoProduto(){
     //Exibe as informações de um produto.
-    int codigo, posicao; //Variavel auxiliar
+    int codigo, posicao = -1; //Variavel auxiliar
+    printf("/n-== Informacoes de um Produto ==-/n");
     printf("Digite o codigo do produto que deseja obter as informacoes: ");
     scanf("%d", &codigo);
-
-    printf("Nome: %s | Preco: %.2f | Codigo: %d", p[posicao].nome, p[posicao].preco, codigo);
+    for (int i = 0; i < totalProdutos; i++){
+        if (p[i].codigo == codigo){
+            posicao = i;
+        }
+    }
+    if (posicao != -1){
+        printf("Nome: %s | Preco: %.2f | Codigo: %d", p[posicao].nome, p[posicao].preco, codigo);
+    } else {
+        printf("Produto nao encontrado.");
+    }
+    
 }
